feec1968495b5df0770e8c55a6edb3d71a380396
commanderINd1E <toast.keks@web.de>
2024-06-01 00:12:17 +0200

    Updates

---
+++ static/js/198.2e97a3fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcapture_the_flag_pwa=self.webpackChunkcapture_the_flag_pwa||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.2e97a3fc.chunk.js.map
